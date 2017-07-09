f942298af745d0d80fc7cf1a48e98ed36b2b880c
takuya <da87435@gmail.com>
2017-07-09 21:04:11 +0900

    add json

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,15 +1,33 @@
 import Kitura
 import HeliumLogger
 import LoggerAPI
+import SwiftyJSON
 
 let router = Router()
 HeliumLogger.use()
 
 router.get("/") {
     request, response, next in
-    Log.info("Hello!Logger!")
-    response.status(.OK).send("Hello Wolrd")
+    Log.info("Hello, Kitura!")
+    response.status(.OK).send("Hello, Kitura!")
     next()
+}
+
+router.get("/test") { request, response, next in
+    try response.send("Hello! Test.").end()
+}
+
+router.get("/json") { (request, response, next) in
+    let id = request.queryParameters["id"] ?? ""
+    let dict: [String: Any] = [
+        "shop": "bood_store",
+        "Item": [
+            "id": "\(id)",
+            "name": "book",
+            "value": "890"
+        ]
+    ]
+    try response.send(json: JSON(dict)).end()
 }
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
